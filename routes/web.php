b4e184380dd680214644a824241915e9a45f2018
herirudini <herirudini@gmail.com>
2025-08-12 21:18:20 +0700

    feat(isAdminMiddleware, web): manage route permission and middleware

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,21 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Middleware\IsAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    Route::resource('categories', CategoryController::class)->middleware(IsAdminMiddleware::class);
 });
 
-Route::resource('categories', CategoryController::class);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
