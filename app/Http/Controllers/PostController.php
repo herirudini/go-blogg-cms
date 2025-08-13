355cf8097182013647173431e73b05dcab646b50
herirudini <herirudini@gmail.com>
2025-08-13 15:04:57 +0700

    feat(CategoryController, PostController, StoreCategoryRequest, categories-add, categories-edit): manage form validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Requests\StorePostRequest;
 
